739064e9633db9d163fc11dfbcb1650dddf2c795
MaxSerebr <56864123+MaxSerebrriacov@users.noreply.github.com>
2021-10-29 23:14:36 +0300

    add pdf

--- custom.sh
+++ custom.sh
@@ -1,12 +1,25 @@
 
-HOST=192.168.60.234
+HOST=192.168.60.216
 
-OID=1.3.6.1.4.1.8072.1.3.2.3.1.1.4.116.101.115.116
+OID2=1.3.6.1.4.1.8072.1.3.2.3.1.1.3.157.151.144
 
-VAL=$(snmpget -v 2c -Ovq -c test $HOST $OID)
-echo "result: ${VAL}"
+VAL=$(snmpget -v 2c -Ovq -c pg $HOST $OID2)
+echo "now: ${VAL}"
+
+
+OID2=1.3.6.1.4.1.8072.1.3.2.3.1.1.4.156.141.155.145
+
+VAL=$(snmpget -v 2c -Ovq -c pg $HOST $OID2)
+echo "now: ${VAL}"
+
+
+OID2=1.3.6.1.4.1.8072.1.3.2.3.1.1.6.162.145.164.165.162.156
+
+VAL=$(snmpget -v 2c -Ovq -c pg $HOST $OID2)
+echo "now: ${VAL}"
+
 
 OID2=1.3.6.1.4.1.8072.1.3.2.3.1.1.3.110.111.119
 
-VAL2=$(snmpget -v 2c -Ovq -c now $HOST $OID2)
-echo "now: ${VAL2}"
+VAL=$(snmpget -v 2c -Ovq -c now $HOST $OID2)
+echo "now: ${VAL}"
